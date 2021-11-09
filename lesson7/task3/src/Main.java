57c0298a770ecfc28b87703ef6bf083517310d01
8malvina8 <zlata.soligorsks@mail.ru>
2021-11-10 01:52:38 +0300

    lesson7 task3 updated

--- lesson7/task3/src/Main.java
+++ lesson7/task3/src/Main.java
@@ -2,93 +2,65 @@ import java.util.Arrays;
 import java.util.OptionalInt;
 
 public class Main {
-    int count = 0;
-    int numSentence;
-    int[] countText;
 
-
-
-   public void PalindromWord(StringBuilder word) {
-       String a = word.toString();
-       StringBuilder str = word.reverse();
-
-       if ((str.toString()).equalsIgnoreCase(a)) {
-           System.out.println("Слово " + a + " является палиндромом");
-                  } else {
-           System.out.println(a+" не палиндром");}
-   }
-
-    public int PalindromCount(StringBuilder sentence) { //определение количества полиндромов в предложении
-
-        String[] words = sentence.toString().split("\\s"); //массив из слов
-        StringBuilder str = sentence.reverse();//переворачиваем всё предложение
-        String[] wordsRevers = str.toString().split("\\s");//массив перевернутых слов
-        int count = 0;
-        for (int i = 0; i < words.length; i++) {
-            for (int j = words.length-1; j > -1; j--)// обратный порядок, т.к. перевернули предложение задом наперед
-                if (words[i].equalsIgnoreCase(wordsRevers[j]) && (words[i].length() > 1)) {
-                    count = count + 1;
-                }
-        }
-        return count;
-
+    public boolean palindromWord(String word) {
+          return (new StringBuilder(word).reverse().toString()).equalsIgnoreCase(word) && word.length() > 1;
     }
 
-    public int PalindromCountX(String str,String strRevers) { //определение количества полиндромов в предложении
+    public int palindromCount(String sentence) { //определение количества полиндромов в предложении
 
-        String[] words = str.toString().split("\\s"); //массив из слов
-        String[] words2 = strRevers.toString().split("\\s");
+        String[] words = sentence.toString().split("\\s"); //массив из слов
 
         int count = 0;
-        for (int i = 0; i < words.length; i++) {
-            for (int j = words2.length-1;j>-1; j--){// обратный порядок, т.к. перевернули предложение задом наперед
-                if (words[i].equalsIgnoreCase(words2[j]) && (words[i].length() > 1)) {
-                    count++;
 
-                }
+        for (int i = 0; i < words.length; i++) {
+            if (palindromWord(words[i])) {
+                count = count + 1;
             }
+
         }
         return count;
+    }
+
+
+    public int palindromCountText(String text) { //определение количества полиндромов в каждом предложении
+
+        String[] sentences = text.toString().split("\\."); //массив из предложений
+
+        int[] count = new int[sentences.length];
+        for (int i = 0; i < sentences.length; i++) {
+            count[i] = palindromCount(sentences[i]);
+        }
+
+        int max = count[0];
+        int nom = 0;
+        for (int i = 1; i < count.length; i++) {
+            if (max < count[i]) {
+                max = count[i];
+                nom = i;
+            }
+        }
+        return nom;
 
     }
 
 
     public static void main(String[] args) {
-          Main obj = new Main();
-          //task3_1
-//        StringBuilder str = new StringBuilder("Анна");
-//        obj.PalindromWord(str);
+        Main obj = new Main();
+        //task3_1
+//        String str = "Анна";
+//        if(obj.PalindromWord(str)) { System.out.println("Слово " + str + " является палиндромом");}
+//        else  System.out.println("Слово " + str + " не палиндром");
 
         //task3_2
-//        StringBuilder str1 = new StringBuilder("Сегодня казак пригласил мадам в ресторан");
+//        String str1 = "Сегодня казак как мам пригласил мадам в ресторан";
 //        System.out.println("Количество палиндромов в предложении: " + obj.PalindromCount(str1));
-
+        int x;
         //task3_3
-        StringBuilder text = new StringBuilder("Сегодня казак пригласил мадам в ресторан. Он позвал как кабак казак её в шалаш." +
-                " Тут мадам возмутилась как так. Топот был как потоп. Дед услышав шум пошел в кабак");
-        String[] sentenceS =text.toString().split("\\.");//предложения
-
-        StringBuilder textRevers=text.reverse();//реверс текста
-        String[] sentenceSRevers =textRevers.toString().split("\\.");//предложения реверса
-
-        int[] CounText = new int[sentenceS.length]; //количесвто палиндромов в каждом предложении
-
-        int j= sentenceSRevers.length-1;
-        for(int i=0; i<sentenceS.length; i++){
-                CounText[i]=obj.PalindromCountX(sentenceS[i],sentenceSRevers[j]);
-                j--;
-            }
-
-        int max=0;
-        int num=0;
-        for (int i=0;i< CounText.length;i++){
-            if(max<CounText[i]){
-                max=CounText[i];
-                num=i+1;
-            }
-        }
-        System.out.println("Номер предложения: "+num);
-        System.out.println("Количество палиндромов: "+CounText[num]);
+        String text = "Сегодня так казак пригласил в ресторан. Он позвал казак так её  в шалаш." +
+                " Тут мадам возмутилась как vfv hgh так. Топот был как потоп. Дед услышав шум пошел в кабак";
+        x = obj.palindromCountText(text);
+        System.out.println("№ предложения с max числом палиндромов: " + (x+1));
 
 
     }
