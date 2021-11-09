d9ed785e65122d9456adb70e19d3ae06968b60b9
8malvina8 <zlata.soligorsks@mail.ru>
2021-11-10 00:33:55 +0300

    lesson8 task3

---
+++ lesson8/task3/src/Test.java
@@ -0,0 +1,24 @@
+import java.io.FileNotFoundException;
+import java.net.URISyntaxException;
+
+public class Test extends Exception{
+    public void test() throws NullPointerException,ArithmeticException, FileNotFoundException, URISyntaxException {
+        throw new NullPointerException();
+
+    }
+
+    public void main(String[] args) throws URISyntaxException,URISyntaxException {
+
+       try {
+            test();
+        }
+        catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+            System.out.println("Error");
+        } catch (NullPointerException ex) {
+            ex.printStackTrace();
+            System.out.println("Error");
+
+       }
+    }
+}
